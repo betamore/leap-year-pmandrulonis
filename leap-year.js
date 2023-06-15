012eb019d4b5167bd03090473f10fc93852abe81
Paul Andrulonis <135282834+pmandrulonis@users.noreply.github.com>
2023-06-15 13:19:32 -0400

    Update leap-year.js

--- leap-year.js
+++ leap-year.js
@@ -13,5 +13,5 @@ function isLeapYear(){
 let years = [ 2015, 1970, 1996, 1960, 2100, 1900, 2000, 2400, 1800]
 
 for (let year of years) {
-	console.log(isLeapYear(year)
+	console.log(isLeapYear(year))
 }
